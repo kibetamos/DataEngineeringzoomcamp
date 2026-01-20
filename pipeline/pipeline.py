638bacc13f6ed07c37ae8495a1603cd77c314c11
kibetamos <amos.rotich@strathmore.edu>
2026-01-20 16:44:27 +0300

    add pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -3,8 +3,15 @@ import pandas as pd
 
 print('arguments', sys.argv)
 
+
+
+df = pd.DataFrame({"day": [1, 2], "number_passengers": [3, 4]})
 month = int(sys.argv[1])
 
-# print("Hello Pipeline")
+print("Hello Pipeline")
 
 print(f'hello pipeline, month= {month}')
+
+print(df.head())
+
+df.to_parquet(f'outut_{month}.parquet')
